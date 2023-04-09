93f0ba2c2d5f2b6ebb8858dfa4d493d0bb9ddefa
avd1ev <avdeev10v@gmail.com>
2023-04-09 21:42:25 +0300

    impl meals Datatables

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,39 @@
+const mealAjaxUrl = "profile/meals/";
+
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable( {
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "date_time"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        })
+    );
+});
